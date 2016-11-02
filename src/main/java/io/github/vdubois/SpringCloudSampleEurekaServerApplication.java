180eaeefc2a1a681c26921efcf4958cf1a26d028
Vincent Dubois <dubois.vct@free.fr>
2016-11-02 21:52:05 +0100

    Initial commit

---
+++ src/main/java/io/github/vdubois/SpringCloudSampleEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package io.github.vdubois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class SpringCloudSampleEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSampleEurekaServerApplication.class, args);
+	}
+}
